4c4a6daf3c27b8614fcc54bbfa72cf39012c2dd8
Nicko Dela Cruz <nickodc96@gamil.com>
2018-03-04 16:04:23 -0800

    add home.erb

--- app.rb
+++ app.rb
@@ -5,4 +5,17 @@ require("./lib/project")
 require("./lib/volunteer")
 require("pg")
 
-DB = PG.connect
+DB = PG.connect({:dbname => 'volunteer_tracker'})
+
+get('/') do
+  @projects = Project.all
+  erb(:home)
+end
+
+post('/') do
+  title = params['title']
+  project = Project.new({:title => title, :id => nil})
+  project.save
+  @projects = Project.all
+  erb(:index)
+end
